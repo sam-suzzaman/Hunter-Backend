2825a4326caab4b261394c23b8c88463bfe02672
samsuzzaman1998 <dksamsuzzaman@gmail.com>
2023-11-17 22:50:30 +0600

    add logout cookie

--- Controller/UserController.js
+++ Controller/UserController.js
@@ -38,8 +38,11 @@ exports.getMe = async (req, res, next) => {
 
 exports.logOut = async (req, res, next) => {
     res.cookie(process.env.COOKIE_NAME, "", {
-        expires: new Date(Date.now()), //expire time of cookie
-        httpOnly: true,
+        expires: new Date(Date.now()),
+        secure: true, // Sent only over HTTPS
+        httpOnly: true, // Restricts access from client-side scripts
+        signed: true, // Helps keep the cookie secure
+        sameSite: "None",
     })
         .status(200)
         .json({ message: "Logout done" });
