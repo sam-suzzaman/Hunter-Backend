ac57686cc0bd3abdaad5350226f09fee469a747f
samsuzzaman1998 <dksamsuzzaman@gmail.com>
2023-11-19 16:58:49 +0600

    test

--- Controller/UserController.js
+++ Controller/UserController.js
@@ -107,8 +107,8 @@ exports.loginUser = async (req, res, next) => {
 
                 const one_day = 1000 * 60 * 60 * 24; //since token expire in 1day
 
-                res.cookie(process.env.COOKIE_NAME, TOKEN, {
-                    expires: new Date(Date.now() + one_day),
+                res.cookie(process.env.COOKIE_NAME, "hello", {
+                    // expires: new Date(Date.now() + one_day),
                     secure: true, // Sent only over HTTPS
                     httpOnly: true, // Restricts access from client-side scripts
                     signed: true, // Helps keep the cookie secure
