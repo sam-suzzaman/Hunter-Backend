3dc4096d38ce8407074ec4c67ca2003ec7f574b8
samsuzzaman1998 <dksamsuzzaman@gmail.com>
2023-11-22 11:27:25 +0600

    add cors methods

--- App.js
+++ App.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(
     cors({
         origin: ["https://hunter-iota.vercel.app", "http://localhost:5173"],
-        methods: ["*"],
+        methods: ["GET,POST,DELETE,PUT,PATCH"],
         credentials: true,
     })
 );
