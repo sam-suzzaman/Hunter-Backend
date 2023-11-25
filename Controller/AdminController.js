c5f3996162f173bd5066f6a2ab88728f5f8c7448
samsuzzaman1998 <dksamsuzzaman@gmail.com>
2023-11-25 11:45:09 +0600

    update job info for admin

--- Controller/AdminController.js
+++ Controller/AdminController.js
@@ -7,10 +7,17 @@ exports.getAllInfo = async (req, res, next) => {
         const admin = await UserModel.find({ role: "admin" });
         const jobs = await JobModel.find({});
 
+        const interviewJobs = await JobModel.find({ jobStatus: "interview" });
+        const pendingJobs = await JobModel.find({ jobStatus: "pending" });
+        const declinedJobs = await JobModel.find({ jobStatus: "declined" });
+
         res.status(200).json({
             user: users?.length,
             job: jobs?.length,
             admin: admin?.length,
+            inverview: interviewJobs?.length,
+            pending: pendingJobs?.length,
+            declined: declinedJobs?.length,
         });
     } catch (error) {
         next(createError(500, error.message));
