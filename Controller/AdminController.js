43a30cfd72c9ff91b201a341d2ca9ee607c72748
samsuzzaman1998 <dksamsuzzaman@gmail.com>
2023-11-25 11:29:50 +0600

    add admin numbers

--- Controller/AdminController.js
+++ Controller/AdminController.js
@@ -4,15 +4,13 @@ const JobModel = require("../Model/JobModel");
 exports.getAllInfo = async (req, res, next) => {
     try {
         const users = await UserModel.find({});
+        const admin = await UserModel.find({ role: "admin" });
         const jobs = await JobModel.find({});
-        const info = {
-            user: users?.length,
-            job: jobs?.length,
-        };
 
         res.status(200).json({
             user: users?.length,
             job: jobs?.length,
+            admin: admin?.length,
         });
     } catch (error) {
         next(createError(500, error.message));
